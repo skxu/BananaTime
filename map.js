b4315e203c709715195211cbbb73439e0eb4050f
skxu-OSX <sxu@airbears2-10-142-37-12.airbears2.1918.berkeley.edu>
2014-09-27 16:04:57 -0700

    initial commit

---
+++ map.js
@@ -0,0 +1,103 @@
+var Map  = {
+  map: null,
+  
+  initialize: function() {
+    
+    var autoOptions = {
+        types: ["geocode"]
+    },
+    autoInput = document.getElementById("locationsearch");
+    autocomplete = new google.maps.places.Autocomplete(autoInput, autoOptions);
+    
+    
+     var mapOptions = {
+      zoom: 18,
+    };
+
+    this.map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+    
+    //If we support geolocation
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = new google.maps.LatLng(position.coords.latitude,
+                                         position.coords.longitude);
+
+        var infowindow = new google.maps.InfoWindow({
+          map: Map.map,
+          position: pos,
+          content: 'Location found using HTML5.'
+        });
+      console.log(position.coords.latitude);
+      console.log(position.coords.longitude);
+      Map.map.setCenter(pos);
+      }, function() {
+        Map.handleNoGeolocation(true);
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      Map.handleNoGeolocation(false);
+    }
+    
+    var markers = [];
+    var input = /** @type {HTMLInputElement} */(
+      document.getElementById('pac-input'));
+    Map.map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    var searchBox = new google.maps.places.SearchBox(
+    /** @type {HTMLInputElement} */(input));
+    
+    
+    
+    google.maps.event.addListener(searchBox, 'places_changed', function() {
+      var places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+      for (var i = 0, marker; marker = markers[i]; i++) {
+        marker.setMap(null);
+      }
+
+      // For each place, get the icon, place name, and location.
+      markers = [];
+      var bounds = new google.maps.LatLngBounds();
+      for (var i = 0, place; place = places[i]; i++) {
+        var image = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Create a marker for each place.
+        var marker = new google.maps.Marker({
+          map: map,
+          icon: image,
+          title: place.name,
+          position: place.geometry.location
+        });
+
+        markers.push(marker);
+
+        bounds.extend(place.geometry.location);
+      }
+
+      map.fitBounds(bounds);
+    });
+    
+  },
+  
+  handleNoGeolocation: function(canHandle) {
+    console.log(canHandle);
+    var pos = new google.maps.LatLng(37.8756081,-122.2587463)
+    var infowindow = new google.maps.InfoWindow({
+          map: Map.map,
+          position: pos,
+          content: 'Location hardcoded.'
+        });
+    
+    Map.map.setCenter(pos);
+  }
+  
+}
